54633a1c86ace5c580ad11c80b72b030346f62af
Minoru Nakano <minoru.nakano@gmail.com>
2022-08-15 12:32:18 -0400

    added tissue and drug annotation

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -1,10 +1,14 @@
 library(stringr)
+library(tibble)
 
 args <- commandArgs(trailingOnly = TRUE)
 input_dir <- args[1]
 output_dir <- args[2]
+annot_dir <- args[3]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/annotate_tissue.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/annotate_drug.R")
 
 clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , header=TRUE )
 rownames(clin) <- clin$geo_accession
@@ -44,6 +48,13 @@ clin$t.os <- unlist(lapply(survival, function(s){
 }))
 
 clin$response = Get_Response( data=clin )
+
+# Tissue and drug annotation
+annotation_tissue <- read.csv(file=file.path(annot_dir, 'curation_tissue.csv'))
+clin <- annotate_tissue(clin=clin, study='Hwang', annotation_tissue=annotation_tissue, check_histo=FALSE)
+
+annotation_drug <- read.csv(file=file.path(annot_dir, 'curation_drug.csv'))
+clin <- add_column(clin, unique_drugid='', .after='unique_tissueid')
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
 
